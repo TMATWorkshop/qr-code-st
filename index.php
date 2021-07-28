15f5faf57dee8abbdfec51cc76f3b1179764fcb5
TMATWorkshop <tmalvin.workshop@gmail.com>
2021-07-28 11:32:34 +0800

    test qr

---
+++ index.php
@@ -0,0 +1,26 @@
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="image/png; charset=utf-8"/>
+    </head>
+<body>
+<script src="qrcode.js"></script>
+<script>
+    function createqr() {
+        var parsedUrl = new URL(window.location.href);
+        var val = 'test';
+        val = parsedUrl.searchParams.get("val");
+		console.log(parsedUrl.searchParams.get("val"));
+
+        var qrcode = new QRCode(document.getElementById("qrcode"), {
+            text: val,
+            width: 128,
+            height: 128,
+            colorDark : "#000000",
+            colorLight : "#ffffff",
+            correctLevel : QRCode.CorrectLevel.H
+        });
+    }
+</script>
+<div id="qrcode"></div>
+</body>
+</html>
