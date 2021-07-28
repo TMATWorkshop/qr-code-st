269e46e1be3c8f0c56f91c7086c851e77154e6fe
TMATWorkshop <tmalvin.workshop@gmail.com>
2021-07-28 13:15:34 +0800

    Composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'd239440ddad94d3806b6829b59b9b654a8bc079a',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'd239440ddad94d3806b6829b59b9b654a8bc079a',
+            'dev_requirement' => false,
+        ),
+    ),
+);
