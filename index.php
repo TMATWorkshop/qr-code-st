aa552a8a66d36bdd59726933719724ae213843a9
TMATWorkshop <tmalvin.workshop@gmail.com>
2021-07-28 11:45:59 +0800

    index

--- index.php
+++ index.php
@@ -1,25 +1,18 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="image/png; charset=utf-8"/>
+        <script src="qrcode.js"></script>
+        <script>
+            function createQR() {
+                var parsedUrl = new URL(window.location.href);
+                var val = 'test';
+                val = parsedUrl.searchParams.get("val");
+                console.log(parsedUrl.searchParams.get("val"));
+                new QRCode(document.getElementById("qrcode"), val);
+            }
+        </script>
     </head>
-<body>
-<script src="qrcode.js"></script>
-<script>
-        var parsedUrl = new URL(window.location.href);
-        var val = 'test';
-        val = parsedUrl.searchParams.get("val");
-		console.log(parsedUrl.searchParams.get("val"));
-
-        var qrcode = new QRCode(document.getElementById("qrcode"), {
-            text: val,
-            width: 128,
-            height: 128,
-            colorDark : "#000000",
-            colorLight : "#ffffff",
-            correctLevel : QRCode.CorrectLevel.H
-        });
-    
-</script>
+<body onload="createQR()">
 <div id="qrcode"></div>
 </body>
 </html>
